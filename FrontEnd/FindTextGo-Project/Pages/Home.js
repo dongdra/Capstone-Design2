0908695d961980cb6721c0d75b8cf9ec8473e276
dongdra <dong000424@naver.com>
2024-09-20 21:41:53 +0900

    BottomNavigation 아이콘 과 높이 수정

--- FrontEnd/FindTextGo-Project/Pages/Home.js
+++ FrontEnd/FindTextGo-Project/Pages/Home.js
@@ -3,6 +3,15 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { BottomNavigation } from 'react-native-paper';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
 const HomeRoute = () => (
   <View style={styles.container}>
     <Text>홈 화면입니다. 로그인 성공!</Text>
@@ -24,9 +33,9 @@ const ProfileRoute = () => (
 export default function Home() {
   const [index, setIndex] = useState(0); // 현재 선택된 탭 인덱스 관리
   const [routes] = useState([
-    { key: 'home', title: '홈', icon: 'home' },
-    { key: 'settings', title: '설정', icon: 'cog' },
-    { key: 'profile', title: '프로필', icon: 'account' },
+    { key: 'profile', title: '프로필', focusedIcon: 'account-circle' },
+    { key: 'home', title: '홈', focusedIcon: 'home' },
+    { key: 'settings', title: '설정', focusedIcon: 'cog' }
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
@@ -40,15 +49,9 @@ export default function Home() {
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
+      barStyle={{ height: 75, backgroundColor: '#ffffff', borderTopWidth: 1, borderTopColor: 'black'}}
     />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-});
+
